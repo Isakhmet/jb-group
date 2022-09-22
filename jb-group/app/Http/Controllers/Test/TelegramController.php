0ea4cc5e14ce62dc3503d4329097863bb19cc2f2
bek <isakhmet.bek@cbc-group.kz>
2022-09-22 16:59:13 +0600

    test tg-bot

--- jb-group/app/Http/Controllers/Test/TelegramController.php
+++ jb-group/app/Http/Controllers/Test/TelegramController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Test;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Telegram\Bot\Api;
 
 class TelegramController extends Controller
 {
     public function getDataFromTg(Request $request)
     {
+        Log::info('tg data:', $request->all());
         return $request->all();
     }
 }
