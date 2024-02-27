13b99b37deb570d9b8582ea4abf7b6709a2d775d
bek <isakhmet.bek@cbc-group.kz>
2024-02-27 16:37:54 +0600

    parser email

---
+++ jb-group/app/Mail/CurrencyEmail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class CurrencyEmail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    private string $fileName;
+
+    /**
+     * CurrencyEmail constructor.
+     * @param string $fileName
+     */
+    public function __construct(string $fileName)
+    {
+        $this->fileName = $fileName;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.currency')
+            ->subject('Subject of the email')
+            ->attach(storage_path('app/public/'.$this->fileName), [
+                'as' => 'currency.xlsx',
+            ])
+            ->with([
+                'key' => 'value',
+            ]);
+    }
+}
