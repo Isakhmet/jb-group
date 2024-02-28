10075d5e1a20a98177a35f63b6c7f37f428f5fda
bek <isakhmet.bek@cbc-group.kz>
2024-02-28 13:45:23 +0600

    parser email

---
+++ jb-group/app/Console/Commands/TestCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Exports\ExchangeExport;
+use App\Mail\CurrencyEmail;
+use Illuminate\Console\Command;
+use App\Services\Parser\MigParser;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Maatwebsite\Excel\Facades\Excel;
+use Matrix\Exception;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:run';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Parser exchanges web sites';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Mail::to(env('MAIL_TO', 'aziaexchange01@mail.ru'))->send(new CurrencyEmail('currencies.xlsx'));
+    }
+}
